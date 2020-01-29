9b47d1c49b061934d96e71cd23914998505d60bf
Marta Koperek <makoperek@gmail.com>
2020-01-29 23:22:46 +0100

    Add waiter/order/new view using MUI

--- src/components/views/Waiter/OrderNew.js
+++ src/components/views/Waiter/OrderNew.js
@@ -1,11 +1,86 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
 import styles from './Waiter.module.scss';
+
+const menu = [
+  {id: 1, dish: 'breakfast', options: ['Late', 'Cappuccino', 'Espresso', 'Macchiato']},
+  {id: 2, dish: 'pizza', options: ['Olives', 'Red peppers', 'Mushrooms', 'Salami']},
+  {id: 3, dish: 'salad', options: ['Cucumber', 'Cheese', 'Fresh herbs', 'Black pepper', 'Olives']},
+];
 
 const OrderNew = () => {
   return (
-    <div className={styles.component}>
-      <h2>Order new view</h2>
-    </div>
+    <Container>
+      <Paper className={styles.component}>
+        <h2>New order</h2>
+        <Grid container spacing={5}>
+          <Grid item xs={6} lg={4} spacing={2}>
+            <Paper>
+              <Typography variant="h5">Table</Typography>
+              <Divider />
+              <TextField id="outlined-basic" label='1' variant="outlined" />
+            </Paper>
+          </Grid>
+          <Grid item xs={6} lg={4} spacing={2}>
+            <Paper>
+              <Typography variant="h5">Value</Typography>
+              <Divider />
+              <Typography variant="h3">123$</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid item xs container direction="column" spacing={5}>
+          <Grid item>
+            <Paper>
+              <Typography variant="h5">Menu</Typography>
+              <Divider />
+              <Table>
+                <TableBody>
+                  {menu.map(row => (
+                    <TableRow key={row.id} className={styles.tableRow}>
+                      <TableCell><Typography variant="h6">{row.dish}</Typography></TableCell>
+                      {row.options.map(option => (
+                        <TableCell key={option}>{option}</TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} lg={12} container direction="column" spacing={5}>
+          <Grid item>
+            <Paper>
+              <Typography variant="h5">Order</Typography>
+              <Divider />
+              <Table>
+                <TableBody>
+                  {menu.map(row => (
+                    <TableRow key={row.id} className={styles.tableRow}>
+                      <TableCell><Typography variant="h6">{row.dish}</Typography></TableCell>
+                      {row.options.map(option => (
+                        <TableCell key={option}>{option}</TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
   );
 };
 
--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -7,6 +7,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
 
 
 const demoContent = [
@@ -24,12 +26,12 @@ const renderActions = status => {
       return (
         <>
           <Button>thinking</Button>
-          <Button>new order</Button>
+          <Button component ={Link} to={`${process.env.PUBLIC_URL}/waiter/order/new`}>new order</Button>
         </>
       );
     case 'thinking':
       return (
-        <Button>new order</Button>
+        <Button component ={Link} to={`${process.env.PUBLIC_URL}/waiter/order/new`}>new order</Button>
       );
     case 'ordered':
       return (
@@ -74,9 +76,7 @@ const Waiter = () => (
             </TableCell>
             <TableCell>
               {row.order && (
-                <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
-                  {row.order}
-                </Button>
+                <Button component ={Link} to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>{row.order}</Button>
               )}
             </TableCell>
             <TableCell>
@@ -90,3 +90,4 @@ const Waiter = () => (
 );
 
 export default Waiter;
+
